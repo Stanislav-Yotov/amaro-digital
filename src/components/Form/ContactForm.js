fd4f77e7c81ff4f61aa457a9e505a9016f094926
Stanislav Yotov <stanislav.iotov@abv.bg>
2022-08-23 13:57:29 +0300

    made some chages

--- src/components/Form/ContactForm.js
+++ src/components/Form/ContactForm.js
@@ -14,25 +14,21 @@ function ContactForm(props) {
                     <h2 className="mb-4 font-weight-medium text-secondary">Do you want to grow your organic traffic and revenue? Tell us your goals.</h2>
                     <div className={styles['row-form-group']}>
                         <div className={styles['first-name']}>
-                            <label className="text-black" htmlFor="fname">First Name</label>
-                            <input type="text" id="fname" className="form-control" />
-                        </div>
-                        <div className={styles['last-name']}>
-                            <label className="text-black" htmlFor="lname">Last Name</label>
-                            <input type="text" id="lname" className="form-control" />
+                            <label className="text-black" htmlFor="fullName">Your Name</label>
+                            <input type="text" id="fullName" className="form-control" placeholder="Full Name"/>
                         </div>
                     </div>
 
                     <div className="row form-group">
                         <div className={styles.email}>
                             <label className="text-black" htmlFor="email">Email</label>
-                            <input type="email" id="email" className="form-control" />
+                            <input type="email" id="email" className="form-control" placeholder="Email Adress" />
                         </div>
                     </div>
                     <div className="row form-group">
                         <div className={styles.message}>
                             <label className="text-black" htmlFor="message">Message</label>
-                            <textarea name="message" id="message" cols="30" rows="5" className="form-control" placeholder="Write your notes or questions here..."></textarea>
+                            <textarea name="message" id="message" cols="30" rows="5" className="form-control" placeholder="Tell us about your project"></textarea>
                         </div>
                     </div>
                     <div className="row form-group mt-4">
